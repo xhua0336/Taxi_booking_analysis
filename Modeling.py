a02d8ff3feafc3595ebf0db8d767d5d4d1a18c78
xhua0336 <64999717+xhua0336@users.noreply.github.com>
2021-04-24 15:31:59 +0800

    import

---
+++ Modeling.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.8.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# numpy
+import numpy as np
+
+
